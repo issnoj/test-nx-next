0a92c26eb7f9ae2b2e9652aec180eb267812a67a
issnoj <issei.nojiri@gmail.com>
2021-12-11 11:54:48 +0900

    add markdown

---
+++ _articles/dynamic-routing.md
@@ -0,0 +1,19 @@
+---
+title: 'Dynamic Routing and Static Generation'
+excerpt: ''
+date: '2021-12-11 00:00:00'
+author:
+  name: issnoj
+---
+
+Commodo ad amet ipsum dolor voluptate mollit magna est nostrud est. Consectetur ut veniam proident voluptate laboris velit. Sunt cupidatat veniam eiusmod consectetur.
+
+## Nisi laboris cillum eu aliqua non duis nisi sit dolore eu.
+
+Commodo aliquip ad magna enim. Nisi adipisicing aliquip amet do adipisicing ut laborum. Laboris ex nulla reprehenderit irure excepteur dolore occaecat ut duis aliquip non. Exercitation culpa id ad occaecat occaecat excepteur aliquip dolore. Officia voluptate duis sit sint cillum ullamco et exercitation velit excepteur mollit ex non cillum. Proident commodo occaecat duis amet duis ullamco cupidatat irure aute excepteur. Consectetur adipisicing aute deserunt proident deserunt id laboris.
+
+## Aute ea quis culpa non labore id adipisicing laborum occaecat elit nostrud.
+
+Magna dolor esse ipsum excepteur laboris ipsum Lorem ex qui pariatur proident ad velit. Laborum amet quis consectetur duis et quis excepteur est aliquip incididunt incididunt exercitation. Ex esse sit aute incididunt amet excepteur consequat incididunt laborum esse exercitation labore pariatur. Nostrud cupidatat quis sit et in velit qui nisi incididunt Lorem cupidatat. Laboris cupidatat elit reprehenderit irure officia dolore consequat aliqua occaecat do cupidatat excepteur nostrud qui. Eiusmod do non culpa irure anim consequat consequat veniam.
+
+Pariatur quis cupidatat ea anim nostrud in veniam id enim officia amet sunt ipsum consectetur. Nisi occaecat aute nulla tempor laboris consequat anim fugiat esse. Anim elit in eu proident labore incididunt aute incididunt incididunt est voluptate do.
--- apps/site/pages/articles/[slug].tsx
+++ apps/site/pages/articles/[slug].tsx
@@ -1,15 +1,29 @@
+import {
+  getParsedFileContentBySlug,
+  renderMarkdown,
+} from '@test-nx-next/markdown';
+import { readdirSync } from 'fs';
 import { GetStaticPaths, GetStaticProps } from 'next';
+import { join } from 'path';
 import { ParsedUrlQuery } from 'querystring';
+import { MDXRemote } from 'next-mdx-remote';
 
 /* eslint-disable-next-line */
 export interface ArticleProps extends ParsedUrlQuery {
   slug: string;
 }
 
-export function Article(props: ArticleProps) {
+const POSTS_PATH = join(process.cwd(), '_articles');
+
+export function Article({ frontMatter, html }: ArticleProps) {
   return (
-    <div>
-      <h1>Visitting, {props.slug}</h1>
+    <div className="m-6">
+      <article className="prose prose-lg">
+        <h1>{frontMatter.title}</h1>
+        <div>by {frontMatter.author.name}</div>
+        <hr />
+        <MDXRemote {...html} />
+      </article>
     </div>
   );
 }
@@ -19,27 +33,21 @@ export const getStaticProps: GetStaticProps<ArticleProps> = async ({
 }: {
   params: ArticleProps;
 }) => {
+  const markdownContent = getParsedFileContentBySlug(params.slug, POSTS_PATH);
+  const html = await renderMarkdown(markdownContent.content);
+
   return {
-    props: {
-      slug: params.slug,
-    },
+    props: { frontMatter: markdownContent.frontMatter, html },
   };
 };
 
 export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
+  const paths = readdirSync(POSTS_PATH)
+    .map((path) => path.replace(/\.mdx?/, ''))
+    .map((slug) => ({ params: { slug } }));
+
   return {
-    paths: [
-      {
-        params: {
-          slug: 'page1',
-        },
-      },
-      {
-        params: {
-          slug: 'page2',
-        },
-      },
-    ],
+    paths,
     fallback: false,
   };
 };
